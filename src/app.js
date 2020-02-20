000ad0a1cc404334cd7a00095ae7e444b04dfdc9
mehtaharsh929 <mehtaharsh929@gmail.com>
2020-02-20 13:01:30 +0530

    404-error and hbs

--- src/app.js
+++ src/app.js
@@ -1,35 +1,40 @@
-const path = require('path')
+const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 const app = express();
-console.log(path.join(__dirname,'../public/'))
-// console.log(__dirname)
-// console.log(__filename)
 
-const publicDirectoryPath= path.join(__dirname,'../public/')
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public/");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
-app.use(express.static(publicDirectoryPath))
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath));
 
-app.set('view engine','hbs')
+//Setup handlers engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    Name: "Harsh Mehta"
+  });
+});
 
-app.get('',(req,res)=>{
-  res.render('index',{
-    title:'Weather App',
-    Name:'Harsh Mehta'
-  })
-})
-
-app.get('/about',(req,res)=>{
-  res.render('about',{
-    Name:"Harsh Mehta",
-    Created_By:"SoluteLabs"
-  })
-})
-app.get('/help',(req,res)=>{
-  res.render('help',{
-    help_text:"This is helpfull text"
-  })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    Name: "Harsh Mehta",
+    Created_By: "SoluteLabs"
+  });
+});
+app.get("/help", (req, res) => {
+  res.render("help", {
+    help_text: "This is helpfull text",
+    Name: "Harsh Mehta"
+  });
+});
 // app.get("", (req, res) => {
 //   res.send("Hello Expres");
 // });
@@ -44,9 +49,25 @@ app.get('/help',(req,res)=>{
 
 app.get("/weather", (req, res) => {
   res.send({
-    forecast:'It is snowing',
-    location:'Philadelphia'
-  })
+    forecast: "It is snowing",
+    location: "Philadelphia"
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: 404,
+    Name: "harsh mehta",
+    errorMerrorMesse: "Help article found"
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: 404,
+    Name: "harsh mehta",
+    errorMerrorMesse: "Page not found"
+  });
 });
 
 app.listen(4545, () => {
